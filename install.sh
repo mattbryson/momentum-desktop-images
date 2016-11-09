0299984b8f156189e2e925eac671934eb4a8c627
Matt Bryson <matt.bryson@rampinteractive.co.uk>
2016-11-09 14:24:16 +0000

    Added the execute flag to the main python script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ CURR_DIR=$(pwd)
 #add the path to the launch agent template
 sed "s|{CURR_DIR}|$CURR_DIR|g" download_agent/me.mattbryson.momentum_download.agent.plist.tpl > download_agent/me.mattbryson.momentum_download.agent.plist
 #copy and install the launch agent
-chmod +x sync_momentum.py && cp download_agent/me.mattbryson.momentum_download.agent.plist ~/Library/LaunchAgents/ && launchctl load  ~/Library/LaunchAgents/me.mattbryson.momentum_download.agent.plist
+cp download_agent/me.mattbryson.momentum_download.agent.plist ~/Library/LaunchAgents/ && launchctl load  ~/Library/LaunchAgents/me.mattbryson.momentum_download.agent.plist
