261d34f154586da1c78623f4847e85d6876474a5
Matt Bryson <matt.bryson@rampinteractive.co.uk>
2017-04-12 17:44:12 +0100

    Imporved installation script so it will prompt the user for the client id, create the launch agent and set up the desktops

--- install.sh
+++ install.sh
@@ -1,6 +1,53 @@
 #get current dir
 CURR_DIR=$(pwd)
-#add the path to the launch agent template
-sed "s|{CURR_DIR}|$CURR_DIR|g" download_agent/me.mattbryson.momentum_download.agent.plist.tpl > download_agent/me.mattbryson.momentum_download.agent.plist
-#copy and install the launch agent
-cp download_agent/me.mattbryson.momentum_download.agent.plist ~/Library/LaunchAgents/ && launchctl load  ~/Library/LaunchAgents/me.mattbryson.momentum_download.agent.plist
+
+BLUE='\033[1;34m'
+GREY='\033[0;37m'
+NC='\033[0m' # No Color
+
+echo "${BLUE}This will set up momentum_sync for you.${NC}"
+echo "First you need to get your momentum client ID \n"
+echo "Press enter to open chrome / momentum and then come back here..."
+
+read $done;
+open /Applications/Google\ Chrome.app/ -nF;
+
+echo "1. In Chrome / Momentum click on the settings cog bottom left"
+echo "2. Click on About"
+echo "3. Double click on the version number under the momentum logo"
+echo "4. Copy the ID that is then shown"
+echo "5. Paste it below..."
+
+read client_id
+
+echo "client_id='$client_id'" > config.py
+
+echo "\n\nGreat, now you can manually run with the command 'python ./sync_momentum.py' \n"
+echo "${BLUE}Would you like to automatically run sync_momentum once a day?${NC}"
+echo "${GREY}you can remove this with ./uninstall.sh later${NC}"
+echo "y/n ?"
+
+read auto
+
+if [ "$auto" == "y" ]; then
+
+  #add the path to the launch agent template
+  sed "s|{CURR_DIR}|$CURR_DIR|g" download_agent/me.mattbryson.momentum_download.agent.plist.tpl > download_agent/me.mattbryson.momentum_download.agent.plist
+  #check launch agent folder exists..
+  mkdir -p ~/Library/LaunchAgents
+  #copy and install the launch agent
+  cp download_agent/me.mattbryson.momentum_download.agent.plist ~/Library/LaunchAgents/ && launchctl load  ~/Library/LaunchAgents/me.mattbryson.momentum_download.agent.plist
+fi
+
+echo "\n\n${BLUE}Awesome, Would you like to set all your desktops to ranomly show these images ?${NC}"
+echo "${GREY}You can chnage this in System Preferences > Desktop and Screensaver${NC}"
+echo "y/n?"
+
+read update
+
+if [ "$update" == "y" ]; then
+  osascript -e 'tell application "System Events" to set picture of every desktop to ("$CURR_DIR/pictures")'
+fi
+
+echo "\nYour all done";
+echo "${GREY}To uninstall you can run ./uninstall.sh${NC}";
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 #Momentum for Mac desktop
-This is a simple python script that will download the Momentum daily images to a folder on your computer, allowing you to point your desktop backgrounds to it.
+This is a simple python script that will download the Momentum daily images to a folder on your computer, and point your desktop backgrounds to it.
 
 #Installation
 Choose a dir on your computer where you want the pictures saved.
@@ -7,40 +7,18 @@ Clone this git repo to that dir
 
     $ cd ~/Pictures
     $ git clone https://github.com/mattbryson/momentum-desktop-images.git
-
-#Configuration
-First you need to get your Momentum Client ID
-
-1. Open a new browser tab to get Momentum loaded up.
-2. Click on the settings cog bottom left
-3. Click on About
-4. Double click on the version number under the momentum logo
-5. Copy the ID that is then shown
-6. Open the `config.py` file
-7. replace `CLIENT_ID` with the copied id
-
-#Manual Run
-Once you have set you `CLIENT_ID` you can run the script to get the current images.
-
-    $ python ./sync_momentum.py
-
-#Automatically Run
-On OSX, you can set up the python script to run once a day, automatically downloading the images.
-
-Run the following shell command form this directory to create the launch agent that will run once a day
-
+    $ cd momentum-desktop-images
     $ ./install.sh
 
-To remove the launch agent, run the uninstall script
+#Uninstallation
 
     $ ./uninstall.sh
 
-#Desktops
-You can then point your Mac desktops to the `pictures` folder that is created to get all the goodness of momentum on your desktop.
 
-If you installed this into your `~/Pictures` dir, then the pictures will be found in
+#Manual Run
+Once you have followed in installation instructions you can manually download the images with this command.
 
-    ~/Pictures/momentum-desktop-images/pictures/
+    $ python ./sync_momentum.py
 
 #Debug
 Run the python script directly to see any potential errors.  
