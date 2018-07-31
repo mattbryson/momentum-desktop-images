5db7ee8888d8a1264b3287c8137cbb117ec37c47
Matt Bryson <matt.bryson@gmail.com>
2018-07-31 12:04:39 +0100

    Added support for Python3

--- sync_momentum.py
+++ sync_momentum.py
@@ -3,23 +3,28 @@
 import os
 import sys
 import json
-import urllib2
-import urllib
 import time
 
+try:
+    # For Python 3.0 and later
+    from urllib.request import Request, urlopen, urlretrieve
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import Request, urlopen, urlretrieve
+    
 from config import *
 
 directory=os.path.dirname(os.path.realpath(__file__)) + '/pictures/'
 today = time.strftime("%Y-%m-%d")
 
-req = urllib2.Request('https://api.momentumdash.com/feed/bulk?syncTypes=backgrounds&localDate=' + today)
+req = Request('https://api.momentumdash.com/feed/bulk?syncTypes=backgrounds&localDate=' + today)
 req.add_header('Host', 'api.momentumdash.com')
 req.add_header('Accept', '*/*')
 req.add_header('X-Momentum-ClientId', client_id)
 req.add_header('X-Momentum-Version', '0.91.1')
 req.add_header('Content-Type', 'application/json')
 
-response = urllib2.urlopen(req)
+response = urlopen(req)
 
 data = json.loads(response.read())
 
@@ -29,7 +34,7 @@ if not os.path.exists(directory):
 for bg in data['backgrounds']:
 	name = bg['filename'].rsplit('/', 1)[-1]
 	path = directory + name+".png"
-	print name
+	print (name)
 	#no file type data, so assume .png
 	if not os.path.exists(path):
-		urllib.urlretrieve(bg['filename'], path)
+		urlretrieve(bg['filename'], path)
